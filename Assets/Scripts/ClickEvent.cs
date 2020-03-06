587c56d9f9365df6610e9d67b5346867a1226e38
StrangeChestnut <strangechestnut@gmail.com>
2020-03-06 15:30:57 +0300

    Added ClickEvent components

---
+++ Assets/Scripts/ClickEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+[Serializable] public class HitEvent : UnityEvent<RaycastHit2D> {}
+
+public class ClickEvent : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    public HitEvent DoHit;
+    private bool _isBlocked = false;
+
+    private bool _isClick;
+
+    public void Stop()
+    {
+        _isBlocked = true;
+    }
+
+    void Update()
+    {
+        if (!_isBlocked && Input.GetMouseButtonDown(0))
+            DoHit?.Invoke(Physics2D.Raycast(_camera.ScreenToWorldPoint(Input.mousePosition), Vector2.zero));
+    }
+}
---
+++ Assets/Scripts/ClickListener.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ClickListener : MonoBehaviour
+{
+    private void Handle(RaycastHit2D hit2D)
+    {
+        if (hit2D.collider == null) return;
+        
+        var executor = hit2D.collider.gameObject.GetComponent<HitExecutor>();
+        if (executor != null) executor.ExecuteAllActions(hit2D);
+    }
+}
